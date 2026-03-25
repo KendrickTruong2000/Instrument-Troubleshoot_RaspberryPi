9663d231e8277f9b387d0b637d4487675f85bdbb
KendrickTruong2000 <kendrick.truongphu2000@gmail.com>
2026-03-25 19:43:47 -0400

    Refactor temperature reading functions and add Fahrenheit

--- Lab10.py
+++ Lab10.py
@@ -5,7 +5,7 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '10-*')[0]
 device_file = device_folder + '/w1_slave'
 
-def read_temp():
+def read_temp_c():
     with open(device_file, 'r') as f:
         lines = f.readlines()
         
@@ -20,8 +20,25 @@ def read_temp():
     
     return temp_c
 
+def read_temp_f():
+    
+    with open(device_file, 'r') as f:
+        lines = f.readlines()
+        
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        with open(device_file, 'r') as f:
+            lines = f.readlines()
+    
+    temp_pos = lines[1].find('t=');
+    temp_string = lines[1][temp_pos+2:]
+    temp_f = (float(temp_string) / 1000.0) * 9.0 / 5.0 + 32.0
+    
+    return temp_f
+    
 while True:
-    print(f"Temperature: {read_temp():.2f} C")
+    print(f"Celsius Temperature: {read_temp_c():.2f} C")
+    print(f"Fahrenheit Temperature: {read_temp_f():.2f} F")
     time.sleep(1)
     
     
